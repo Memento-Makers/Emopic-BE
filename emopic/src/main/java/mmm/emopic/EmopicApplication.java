2f866ca75d239ae806807114700f59debbde458b
SSH9753 <kor159753@korea.ac.kr>
2023-08-17 21:10:54 +0900

    🔧env: Spring boot 서버 생성

---
+++ emopic/src/main/java/mmm/emopic/EmopicApplication.java
@@ -0,0 +1,13 @@
+package mmm.emopic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmopicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmopicApplication.class, args);
+	}
+
+}
---
+++ emopic/src/test/java/mmm/emopic/EmopicApplicationTests.java
@@ -0,0 +1,13 @@
+package mmm.emopic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmopicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
