7441380156f106ae0324220f418bd05b010d154c
voka <chdlswhd7@naver.com>
2023-08-22 00:36:08 +0900

    feat: 이미지 업로드 API Dto 및 Controller 개발

---
+++ emopic/src/main/java/mmm/emopic/app/domain/photo/PhotoController.java
@@ -0,0 +1,28 @@
+package mmm.emopic.app.domain.photo;
+
+import lombok.RequiredArgsConstructor;
+import mmm.emopic.app.base.Dto.BaseResponse;
+import mmm.emopic.app.domain.photo.dto.PhotoUploadRequest;
+import mmm.emopic.app.domain.photo.dto.PhotoUploadResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api") //
+public class PhotoController {
+    private final PhotoService photoService;
+
+    //이미지 업로드
+    @PostMapping("/photos")
+    public ResponseEntity createPhoto(@Validated @RequestBody PhotoUploadRequest photoUploadRequest){
+        PhotoUploadResponse response = photoService.createPhoto(photoUploadRequest);
+        return ResponseEntity.ok(new BaseResponse( HttpStatus.OK.value(), "이미지 업로드 signed_url 생성 성공",response));
+    }
+
+}
---
+++ emopic/src/main/java/mmm/emopic/app/domain/photo/dto/PhotoUploadRequest.java
@@ -0,0 +1,20 @@
+package mmm.emopic.app.domain.photo.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import mmm.emopic.app.domain.photo.Photo;
+
+import javax.validation.constraints.NotBlank;
+@Getter
+@NoArgsConstructor
+public class PhotoUploadRequest {
+
+    @NotBlank
+    private String fileName;
+
+    public Photo toEntity() {
+        return Photo.builder()
+                .name(fileName)
+                .build();
+    }
+}
