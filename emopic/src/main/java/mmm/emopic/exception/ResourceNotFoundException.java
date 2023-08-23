a2857372611e8fc9eeb4af3fd63d173d2dc4c612
SSH9753 <kor159753@korea.ac.kr>
2023-08-23 21:34:32 +0900

    🔧env      :  error detecting 을 위한 exception format

---
+++ emopic/src/main/java/mmm/emopic/exception/ResourceNotFoundException.java
@@ -0,0 +1,22 @@
+package mmm.emopic.exception;
+
+public class ResourceNotFoundException extends BadRequestException {
+    private static final String ERROR_CODE = "BAD_REQUEST";
+    private static final String SERVER_MESSAGE = "존재하지 않는 객체 조회";
+
+    public ResourceNotFoundException(String domain, final Long id) {
+        super(String.format("%s %s id : %d", domain, SERVER_MESSAGE, id), String.format("%s %s id : %d", domain, SERVER_MESSAGE, id), ERROR_CODE);
+    }
+
+    public ResourceNotFoundException(String domain, final Long id1, final Long id2, final Long id3) {
+        super(String.format("%s %s id : %d, %d, %d", domain, SERVER_MESSAGE, id1, id2, id3), String.format("%s %s id : %d %d %d", domain, SERVER_MESSAGE, id1, id2, id3), ERROR_CODE);
+    }
+
+    public ResourceNotFoundException(String domain, final String code) {
+        super(String.format("%s %s code : %s", domain, SERVER_MESSAGE, code), String.format("%s %s url : %s", domain, SERVER_MESSAGE, code), ERROR_CODE);
+    }
+
+    public ResourceNotFoundException(String reason) {
+        super(SERVER_MESSAGE, reason, ERROR_CODE);
+    }
+}
