f9aafb01cad1eb5b7ba9f5f3190bb4d1d9a982a8
voka <chdlswhd7@naver.com>
2023-08-24 22:33:14 +0900

    ✨feat: 로그인 Request, Response 정의 #12

--- emopic/src/main/java/mmm/emopic/app/auth/Dto/LoginRequest.java
+++ emopic/src/main/java/mmm/emopic/app/auth/Dto/LoginRequest.java
@@ -1,4 +1,15 @@
 package mmm.emopic.app.auth.Dto;
 
+import lombok.Getter;
+
+@Getter
 public class LoginRequest {
+
+    String userId;
+    String password;
+
+    public LoginRequest(String userId, String password) {
+        this.userId = userId;
+        this.password = password;
+    }
 }
