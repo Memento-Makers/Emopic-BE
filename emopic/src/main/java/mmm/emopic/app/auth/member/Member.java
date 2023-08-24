7d4e70038b8d528aa7d4b7bcf6494c02358bf661
voka <chdlswhd7@naver.com>
2023-08-24 21:44:44 +0900

    ✨feat: Member 엔티티 추가

---
+++ emopic/src/main/java/mmm/emopic/app/auth/member/Member.java
@@ -0,0 +1,38 @@
+package mmm.emopic.app.auth.member;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import mmm.emopic.app.auth.member.support.Provider;
+import mmm.emopic.app.auth.member.support.Role;
+import mmm.emopic.app.base.BaseEntity;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userId;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    private Provider provider;
+
+    @Builder
+    public Member(String userId, String password, Role role, Provider provider) {
+        this.userId = userId;
+        this.password = password;
+        this.role = role;
+        this.provider = provider;
+    }
+}
---
+++ emopic/src/main/java/mmm/emopic/app/auth/member/support/Role.java
@@ -0,0 +1,5 @@
+package mmm.emopic.app.auth.member.support;
+
+public enum Role {
+    USER,ADMIN
+}
