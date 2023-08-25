73b8594fbcee84376f44f7bb1b55ebc416e87bdf
voka <chdlswhd7@naver.com>
2023-08-25 19:55:54 +0900

    ✨feat: 회원가입 service 로직 개발 #12

--- emopic/src/main/java/mmm/emopic/app/auth/member/MemberRepository.java
+++ emopic/src/main/java/mmm/emopic/app/auth/member/MemberRepository.java
@@ -6,4 +6,6 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByUserId(String userId);
+
+    Boolean existsByUserId(String userId);
 }
