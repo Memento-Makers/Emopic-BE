17bdc795ce9c3989652afb3b5ccafcb7cd75cf0c
SSH9753 <kor159753@korea.ac.kr>
2023-09-06 18:44:04 +0900

    ♻️refactor : ReGenerate -> isExpired 함수명 변경

--- emopic/src/main/java/mmm/emopic/app/domain/photo/support/SignedURLReGenerator.java
+++ emopic/src/main/java/mmm/emopic/app/domain/photo/support/SignedURLReGenerator.java
@@ -2,13 +2,10 @@ package mmm.emopic.app.domain.photo.support;
 
 import lombok.Getter;
 import mmm.emopic.app.domain.photo.Photo;
-import net.bytebuddy.asm.Advice;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.Month;
 import java.util.Optional;
 
 @Getter
@@ -17,10 +14,9 @@ public class SignedURLReGenerator {
     @Value("${DURATION}")
     private Long duration;
 
-    public boolean ReGenerate(Photo photo){
-        Optional<LocalDateTime> oldSignedUrlCreateTime = Optional.ofNullable(photo.getSignedUrlCreateTime());
+    public boolean isExpired(Photo photo){
+        Optional<LocalDateTime> oldSignedUrlCreateTime = Optional.ofNullable(photo.getSignedUrlExpireTime());
         if(oldSignedUrlCreateTime.isEmpty()) return true;
-        oldSignedUrlCreateTime.get().plusMinutes(duration);
-        return LocalDateTime.now().isAfter(oldSignedUrlCreateTime.get());
+        return LocalDateTime.now().isAfter(photo.getSignedUrlExpireTime());
     }
 }
