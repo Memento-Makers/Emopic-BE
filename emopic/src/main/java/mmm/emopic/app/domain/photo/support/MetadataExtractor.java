8a3bb699dc6e75d61ca25bbfd8444d161713d1a8
voka <chdlswhd7@naver.com>
2023-10-17 23:55:00 +0900

    feat: 메타 데이터 추출 라이브러리 추가 및 Optional 객체 사용해서 안전하게 데이터 반환하도록 기능 작성 #49

---
+++ emopic/src/main/java/mmm/emopic/app/domain/photo/support/MetadataExtractor.java
@@ -0,0 +1,55 @@
+package mmm.emopic.app.domain.photo.support;
+
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.exif.ExifSubIFDDirectory;
+import com.drew.metadata.exif.GpsDirectory;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Point;
+import org.locationtech.jts.geom.PrecisionModel;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.Optional;
+
+@Configuration
+public class MetadataExtractor {
+
+    public Optional<Date> getSnappedDate(Metadata metadata){
+        if(metadata.containsDirectoryOfType(ExifSubIFDDirectory.class)) {
+            ExifSubIFDDirectory exif = metadata.getFirstDirectoryOfType(ExifSubIFDDirectory.class);
+            return Optional.of(exif.getDateOriginal());
+        }
+        return Optional.empty();
+    }
+
+    public Optional<Point> getLocationPoint(Metadata metadata){
+        GpsDirectory gps = metadata.getFirstDirectoryOfType(GpsDirectory.class);
+        if(metadata.containsDirectoryOfType(GpsDirectory.class)){
+            if(gps.containsTag(GpsDirectory.TAG_LATITUDE) && gps.containsTag(GpsDirectory.TAG_LONGITUDE)){
+                //지리적 위치 데이터를 저장하는 데 사용되는 표준 좌표계 중 하나인 WGS84(World Geodetic System 1984)를 기반으로 하는 모델
+                GeometryFactory geometryFactory = new GeometryFactory(new PrecisionModel(), 4326);
+                return Optional.of(geometryFactory.createPoint(new Coordinate(gps.getGeoLocation().getLatitude(),gps.getGeoLocation().getLongitude())));
+            }
+        }
+        return Optional.empty();
+    }
+
+
+    public Optional<Metadata> readMetadata(MultipartFile image){
+        try{
+            return Optional.of(ImageMetadataReader.readMetadata(image.getInputStream()));
+        }
+        catch (ImageProcessingException e) {
+            System.out.println(e);
+            return Optional.empty();
+        } catch (IOException e) {
+            System.out.println(e);
+            return Optional.empty();
+        }
+    }
+}
