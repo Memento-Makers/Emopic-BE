58359d42cc9475afb2c0483244cf1c01d63353c7
voka <chdlswhd7@naver.com>
2023-11-04 20:27:10 +0900

    update: diary 조회 API에 caption 내용 반환하는 필드 추가

--- emopic/src/main/java/mmm/emopic/app/domain/diary/dto/response/DiaryGetResponse.java
+++ emopic/src/main/java/mmm/emopic/app/domain/diary/dto/response/DiaryGetResponse.java
@@ -1,12 +1,17 @@
 package mmm.emopic.app.domain.diary.dto.response;
 
+import lombok.Builder;
 import lombok.Getter;
 import mmm.emopic.app.domain.diary.Diary;
 
 @Getter
 public class DiaryGetResponse {
     private String diary;
-    public DiaryGetResponse(Diary diary){
-        this.diary = diary.getContent();
+    private String caption;
+
+    @Builder
+    public DiaryGetResponse(String diary, String caption) {
+        this.diary = diary;
+        this.caption = caption;
     }
 }
